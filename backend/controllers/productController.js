06fe67eeeeb6a28af93199060ef7407c08affbad
nurdinfx <nuurdiinxaliimo@gmail.com>
2026-03-14 15:36:32 +0300

    Fix: Export 'deleteProduct' is not defined in productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -153,6 +153,21 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.deleteOne();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 // @desc    Create new review
 // @route   POST /api/products/:id/reviews
 // @access  Private
